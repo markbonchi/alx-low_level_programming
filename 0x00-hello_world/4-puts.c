4ca806c2e9082adf161d841f189f5a0b7b3c49dd
Mark Obi <markbonchi@gmail.com>
2022-06-30 13:09:53 -0700

    Update to C file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 
-int main(void)
+/**
+ * function_name: main
+ *
+ * Description: prints string 
+ * Return: 0
+ */
+
+
+int main (void)
 {
-  puts("Programming is like building a multilingual puzzle\n");
+  puts("\"Programming is like building a multilingual puzzle\n");
   return(0);
 }
