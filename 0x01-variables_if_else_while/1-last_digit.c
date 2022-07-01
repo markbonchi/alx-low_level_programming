bbf42c56b748b0056fb68961f741d985c9689ca0
Mark Obi <markbonchi@gmail.com>
2022-07-01 12:10:14 -0700

    C file 1-last_digit added

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -20,7 +20,7 @@ int main(void)
 	if (mod > 5)
 	{
 		printf("Last digit of %d is %d and is greater than 5\n", n, mod);
-	} else if (mod == o)
+	} else if (mod == 0)
 	{
 		printf("Last digit of %d is %d and is 0", n, mod);
 	} else
