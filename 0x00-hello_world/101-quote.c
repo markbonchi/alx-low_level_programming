95e99d6262ee23c3a9488f8a4b01049ccb569c6f
Mark Obi <markbonchi@gmail.com>
2022-07-01 04:24:25 -0700

    New C file added

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -11,7 +11,6 @@
 
 int main(void)
 {
-	write(STDERR_FILENO, "and that piece of art is useful\" - Dora Korpar,\
-		       	2015-10-19", 59);
+	write(STDERR_FILENO, "and that piece of art is useful\" - Dora Korpar,2015-10-19", 59);
 	return (1);
 }
