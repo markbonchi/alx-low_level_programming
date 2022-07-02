3e64e6697699ddc5b972a94bb821437941c657e4
Mark Obi <markbonchi@gmail.com>
2022-07-01 19:07:03 -0700

    c file 9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Enter
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		putchar(i);
+		putchar(',' + ' ');
+	}
+	putchar('\n');
+	return (0);
+}
