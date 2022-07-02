acfd59abd4fdde483a266853098664147baaccef
Mark Obi <markbonchi@gmail.com>
2022-07-01 17:56:09 -0700

    C file 7-print_tebahpla.c added

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * main - Enter
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	const char a[] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = (int)strlen(a) - 1; i > 0; i--)
+	{
+		putchar(a[i]);
+	}
+	putchar('\n');
+	return (0);
+}
