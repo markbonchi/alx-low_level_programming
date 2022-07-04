1f76338d0fe4334cce865f7e39a89b465f73a852
Mark Obi <markbonchi@gmail.com>
2022-07-04 03:15:53 -0700

    New C file 100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Enter
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+	int j;
+	char a;
+	char b;
+
+	for (i = '0'; i < '9'; i++)
+	{
+		for (j = '1'; j < '9'; j++)
+		{
+			a = ij;
+			b = ji;
+			if (a != b)
+			{
+				putchar(i);
+				putchar(j);
+				if (ij == 89)
+				{
+					break;
+				}
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
