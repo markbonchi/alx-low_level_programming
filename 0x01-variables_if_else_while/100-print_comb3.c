66fe2d6a80e6964b256e9320308673ae47ee4098
Mark Obi <markbonchi@gmail.com>
2022-07-04 05:05:27 -0700

    Update to 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -16,14 +16,15 @@ int main(void)
 	{
 		for (j = '1'; j <= '9'; j++)
 		{
-			if (i == '9')
+			if (i < j)
 			{
-				break;
-			} else {
-				if (i < j)
+				putchar(i);
+				putchar(j);
+				if (i == '8' && j == '9')
 				{
-					putchar(i);
-					putchar(j);
+					continue;
+				} else
+				{
 					putchar(',');
 					putchar(' ');
 				}
