48d0c27f9d2d1ca1cdb4ce2e3c33f4b65eee7a51
Mark Obi <markbonchi@gmail.com>
2022-07-04 09:57:57 -0700

    New C file added

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Enter
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+	int j;
+	int k;
+	int y;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		for (j = '0'; j < '9'; j++)
+		{
+			for (k = '0'; k <= '9'; k++)
+			{
+				for (y = '1'; y <= '9'; y++)
+				{
+					if (i <= k && j < y)
+					{
+						putchar(i);
+						putchar(j);
+						putchar(' ');
+						putchar(k);
+						putchar(y);
+						if (i == '9' && j == '8' && k == '9' && y == '9')
+						{
+							break;
+						} else
+						{
+							putchar(',');
+							putchar(' ');
+						}
+					} else
+					{
+						continue;
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
