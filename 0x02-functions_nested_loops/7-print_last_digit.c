58842b164596c734e7a2d2015f37dc956ee01dd4
Mark Obi <markbonchi@gmail.com>
2022-07-05 15:34:54 -0700

    Update to header

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -3,12 +3,12 @@
 /**
  * print_last_digit - function to to print last digit
  * @n: int argument of function for int
- * Return: n
+ * Return: i
  */
 
 int print_last_digit(int n)
 {
 	int i = n % 10;
 
-	return(n);
+	return(i);
 }
