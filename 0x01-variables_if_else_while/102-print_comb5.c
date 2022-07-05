c441fdb1303da06796196fd4230069497d88df2f
Mark Obi <markbonchi@gmail.com>
2022-07-04 19:17:41 -0700

    test for 102

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -12,9 +12,9 @@ int main(void)
 	int i = '0', j = '0';
 	int k = '0', y = '0';
 
-	while (i <= '9', j <= '9')
+	while (i <= '9' && j <= '9')
 	{
-		while (k <= '9', y <= '9')
+		while (k <= '9' && y <= '9')
 		{
 			putchar(i);
 			putchar(j);
