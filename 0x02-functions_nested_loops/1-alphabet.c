944cdf8ad4b623fa8482411457ee48fd124c7420
Mark Obi <markbonchi@gmail.com>
2022-07-05 13:24:43 -0700

    New C file added

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+
+/**
+ * print_alphabet - prints letters in lowercase
+ *
+ * Return: 0 (Success)
+ */
+
+int print_alphabet(void)
+{
+	int i;
+	char alpha[] = "abcdefghijklmnopqrstuvwxyz";
+
+	for (i = 0; i < 27; i++)
+	{
+		_putchar(alpha[i]);
+	}
+	_putchar('\n');
+	return (0);
+}
