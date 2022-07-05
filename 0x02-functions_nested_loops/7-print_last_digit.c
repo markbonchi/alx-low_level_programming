34f2495aa6b68daf5f371253a4dfc1c65f680327
Mark Obi <markbonchi@gmail.com>
2022-07-05 15:31:52 -0700

    Update to header

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_last_digit - function to to print last digit
+ * @n: int argument of function for int
+ * Return: 0
+ */
+
+int print_last_digit(int n)
+{
+	int i = n % 10;
+
+	_putchar(i);
+	return (0);
+}
