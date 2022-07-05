dd8bbcb177dcd83cced8039ea2a7c42cc4b4f406
Mark Obi <markbonchi@gmail.com>
2022-07-04 18:54:53 -0700

    Update to 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -12,9 +12,9 @@ int main(void)
 	int i, j;
 	int k, y;
 
-	for (i = '0', j = '0'; i <= '9', j <= '9'; i++, j++)
+	for (i = '0', j = '0'; i <= '9' && j <= '9'; i++, j++)
 	{
-		for (k = '0', y = '0'; k <= '9', y <= '9'; k++, y++)
+		for (k = '0', y = '0'; k <= '9' && y <= '9'; k++, y++)
 		{
 			putchar(i);
 			putchar(j);
