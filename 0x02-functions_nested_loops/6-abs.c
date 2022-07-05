1c2298b2284ea3ceb6b8ce9df532d8f5735aa69a
Mark Obi <markbonchi@gmail.com>
2022-07-05 15:10:12 -0700

    Update to header

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _abs - function to return absolut valure of int
+ *
+ * Return: 0
+ */
+
+int _abs(int)
+{
+	int n;
+
+	if (n >= 0)
+	{
+		return (n);
+	} else
+	{
+		return(n * -1);
+	}
+	return (0);
+}
