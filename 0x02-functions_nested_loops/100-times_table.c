9cc25b95a7f1ae34c2e1b05dadc708f46b3052fe
Mark Obi <markbonchi@gmail.com>
2022-07-05 19:13:45 -0700

    Update to header

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -13,9 +13,9 @@ void print_times_table(int n)
 
 	if (n >= 0 && n <= 15)
 	{
-		for (i = 0; i <= 9; i++)
+		for (i = 0; i <= n; i++)
 		{
-			for (j = 0; j <= 9; j++)
+			for (j = 0; j <= n; j++)
 			{
 				k = i * j;
 				if (j == 0)
