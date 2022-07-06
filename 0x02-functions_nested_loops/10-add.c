be227ee8bdcd8ad52c1a8c3ee75460265a8cf428
Mark Obi <markbonchi@gmail.com>
2022-07-05 18:11:03 -0700

    Update to header

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - function to sum two integers
+ * @a: int argument of function for addition
+ * @b: int argument of function for addition
+ * Return: 0
+ */
+
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+	_putchar(c + '0');
+	return (c); 
+}
