4d149b45de95251c50f49f9ed9d93aca321e5307
Mark Obi <markbonchi@gmail.com>
2022-07-05 18:42:45 -0700

    Update to header

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - prints numbers to _98
+ *
+ * Return: 0
+ */
+
+void times_table(int n)
+{
+	int i;
+
+	if (n <= 98)
+	{
+		for (i = n; i <= 98; i++)
+		{
+			if (i != 98)
+			{
+				printf("%d, ", i);
+			} else
+			{
+				printf("%d\n", i);
+			}
+		}
+	} else if (n >= 98)
+	{
+		for (i = n; i >= 98; i--)
+		{
+			if (i != 98)
+			{
+				printf("%d, ", i);
+			} else
+			{
+				printf("%d\n");
+			}
+		}
+	}
+}
