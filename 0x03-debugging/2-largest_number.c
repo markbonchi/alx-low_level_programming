57293243591251f17ff51c25e323d3656b04846a
Mark Obi <markbonchi@gmail.com>
2022-07-06 13:25:12 -0700

    Update to header

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -15,10 +15,10 @@ int largest_number(int a, int b, int c)
 	if (a > b && b > c)
 	{
 		largest = a;
-	} else if (b > a && b > c)
+	} else if (b > a && a > c)
 	{
 		largest = b;
-	} else if (c > a && c > b)
+	} else if (c > a && a > b)
 	{
 		largest = c;
 	}
