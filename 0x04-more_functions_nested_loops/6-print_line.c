d663fef49b6a04460a192c45d5af7ed71410ea2c
Mark Obi <markbonchi@gmail.com>
2022-07-07 12:35:10 -0700

    Update to header and function file

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_line - as the name implies
+ * @n: int argument of function for unit length of line
+ * Return: void
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i <= n; i++)
+	{
+		if (i > 0)
+		{
+			putchar('_');
+		}
+	}
+	_putchar('\n');
+}
