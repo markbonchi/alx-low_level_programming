d1b4b8b23cb1977068340160497c1be47b619c24
Mark Obi <markbonchi@gmail.com>
2022-07-07 09:14:28 -0700

    Update to functions

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isupper - checks for upper case characters
+ * @c - int argument of function for ASCII comparison
+ * Return: 1 if upper, 0 if no match
+ */
+
+int _isupper(int c)
+{
+	if (c >= 'A' && cc <= 'Z')
+	{
+		return (1);
+	} else
+	{
+		return (0);
+	}
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,3 @@
+#0x04-more_functions_nested_loops
+
+This directory contains project files for 0x04-more_functions_nested_loops
