6ec925379b2c39bf06123bf43012c3d4189673ef
Mark Obi <markbonchi@gmail.com>
2022-07-07 12:20:35 -0700

    Update to header and function file

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints numbers from 0 - 14
+ *
+ * Return: void
+ */
+
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = '0'; i <= '1'; i++)
+	{
+		for (j = '0'; j <= '9'; j++)
+		{
+			if (i > '0')
+			{
+				_putchar(i);
+			}
+			_putchar(j);
+			if (j == '4')
+			{
+				break;
+			}
+		}
+	}
+
+	_putchar('\n');
+}
