edbc1700d643e9217011b81d9fc3717318bc4e62
Mark Obi <markbonchi@gmail.com>
2022-07-07 10:01:21 -0700

    Update to function and header

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks for upper case characters
+ * @a: first int argument of function
+ * @b: second int argument for function
+ * Return: i
+ */
+
+int mul(int a, int b)
+{
+	int i;
+
+	i = a * b;
+	return (i);
+}
