65615f571ea67fef696def35865a8adf639673bf
Mark Obi <markbonchi@gmail.com>
2022-07-07 12:25:22 -0700

    Update to header and function file

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,23 +8,25 @@
 
 void more_numbers(void)
 {
-	int i, j;
+	int i, j, k;
 
-	for (i = '0'; i <= '1'; i++)
+	for (k = 0; k < 10; k++)
 	{
-		for (j = '0'; j <= '9'; j++)
+		for (i = '0'; i <= '1'; i++)
 		{
-			if (i > '0')
+			for (j = '0'; j <= '9'; j++)
 			{
-				_putchar(i);
-			}
-			_putchar(j);
-			if (j == '4')
-			{
-				break;
+				if (i > '0')
+				{
+					_putchar(i);
+				}
+				_putchar(j);
+				if (j == '4')
+				{
+					break;
+				}
 			}
 		}
+		_putchar('\n');
 	}
-
-	_putchar('\n');
 }
