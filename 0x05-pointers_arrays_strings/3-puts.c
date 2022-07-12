aa622198032c322bbfca62defdeed00e3a8fbfd9
Mark Obi <markbonchi@gmail.com>
2022-07-12 15:30:20 -0700

    Update to header and function

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _puts - print out string
+ * @str: string pointer variable
+ */
+
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(str)
+		str++;
+	}
+}
