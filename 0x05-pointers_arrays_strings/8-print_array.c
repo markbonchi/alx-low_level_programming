51721cece56189e5082cb6a298a7c7da8337a085
Mark Obi <markbonchi@gmail.com>
2022-07-12 21:29:45 -0700

    Update to header and function file

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_array - print elements of an array
+ * @a: int pointer var of array
+ * @n: number of elements to print
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i <= n; i++)
+	{
+		if (i == n)
+		{
+			printf("%d", a[i]);
+		}
+		printf("%d, ", a[i]);
+	}
+}
