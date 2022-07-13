128d6a9d25cb65c01dc51654a8b430888519e576
Mark Obi <markbonchi@gmail.com>
2022-07-12 18:43:49 -0700

    Update to header and function file

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * rev_string - print string in reverse
+ * @s: pointer variable for string
+ */
+
+void rev_string(char *s)
+{
+	int i, j;
+	char rev[];
+
+	for (i = strlen(s) - 1; i >= 0; i--)
+	{
+		j = 0;
+		while (j < strlen(s))
+		{
+			rev[j] = s[i];
+		}
+	}
+	printf("%s\n", rev);
+}
