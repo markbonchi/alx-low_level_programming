1a71213e46dad090f83b271b0494d8545f8e502e
Mark Obi <markbonchi@gmail.com>
2022-07-12 17:31:45 -0700

    Update to header and function file

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -15,9 +15,9 @@ void print_rev(char *s)
 		len++;
 		s++;
 	}
-	for (i = len - 1; i >= 0; i--)
+	for (i = 0; i < len; i++)
 	{
-		_putchar(*(s + i));
+		_putchar(s[len - i]);
 	}
 	_putchar('\n');
 }
