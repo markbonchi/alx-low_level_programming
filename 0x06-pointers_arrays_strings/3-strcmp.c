1ca8d6029fa182820dd71a71a4b98c67e7f1f1a9
Mark Obi <markbonchi@gmail.com>
2022-07-14 12:27:03 -0700

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -16,14 +16,16 @@ int _strcmp(char *s1, char *s2)
 	{
 		i++;
 	}
-	if (s1[i] == '\0' && s2[i] == '\0')
+	if (s1[i] != '\0' || s2[i] != '\0')
 	{
-		return (0);
-	} else if (s1[i] == '\0')
-	{
-		return (-15);
-	} else
-	{
-		return (15);
+		if (s1[i] != '\0')
+		{
+			return (15);
+		}
+		if (s2[i] != '\0')
+		{
+			return (-15);
+		}
 	}
+	return (0);
 }
