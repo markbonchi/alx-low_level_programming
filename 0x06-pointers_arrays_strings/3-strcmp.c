0df7bf6233ff802f90ad01712cf3c8a6fdd494b4
Mark Obi <markbonchi@gmail.com>
2022-07-14 12:14:23 -0700

    strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strcmp - compare two strings
+ * @s1: first string
+ * @s2: second string
+ * Return: 0 + 15 || 0 || 0 - 15
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	i = 0;
+	while (s1[i] != '\0' && s2[i] != '\0')
+	{
+		i++;
+	}
+	if (s1[i] == '\0' && s2[i] == '\0')
+	{
+		return (0);
+	} else if (s1[i] == '\0')
+	{
+		return (-15);
+	} else
+	{
+		return (15);
+	}
+}
