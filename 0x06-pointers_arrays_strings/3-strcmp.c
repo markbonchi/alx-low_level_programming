01eefeddeb02f7ccebf437ba78414ecb4d36938a
Mark Obi <markbonchi@gmail.com>
2022-07-14 19:41:38 -0700

    Update to strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -18,11 +18,11 @@ int _strcmp(char *s1, char *s2)
 	}
 	if (s1[i] != '\0' || s2[i] != '\0')
 	{
-		if (s1[i] != '\0')
+		if (s1[i] != '\0' && s1 != 0)
 		{
 			return (15);
 		}
-		if (s2[i] != '\0')
+		if (s2[i] != '\0' && s2 != 0)
 		{
 			return (-15);
 		}
