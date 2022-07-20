cb6bda504724d11245e386758f8345dc41378082
Mark Obi <markbonchi@gmail.com>
2022-07-20 12:56:21 -0700

    _puts_recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - Prints a string
+ * @s: pointer to string argument
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(s[0]);
+	_puts_recursion(s + 1);
+}
