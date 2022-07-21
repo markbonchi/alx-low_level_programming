403d2541f2de431b70b8b80559793151d3e1c4e3
Mark Obi <markbonchi@gmail.com>
2022-07-20 21:24:03 -0700

    is_prime_number

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * is_prime_number - check if input iniger is a prime number
+ * @n: input int parameter
+ * @val: counter parameter
+ * Return: prime
+ */
+
+int prime(int n, int val);
+
+int is_prime_number(int n)
+{
+	return (prime(n, 1));
+}
+
+int prime(int n, int val)
+{
+	int i, j;
+
+	i = (6 * val) - 1;
+	j = (6 * val) + 1;
+	if ((i == n || j == n) && n > 1)
+	{
+		if (n % 3 != 0 && n % 5 != 0)
+		{
+			return (1);
+		}
+	}
+	if (j < n)
+	{
+		return (prime(n, val + 1));
+	}
+	return (0);
+}
