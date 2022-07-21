2c31eac1bac33d9e43e87980314efb3d7d1f23ac
Mark Obi <markbonchi@gmail.com>
2022-07-20 17:16:41 -0700

    Function _pow_recursion

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,6 +1,10 @@
 #include "main.h"
 
 /**
+ * _pow_recursion - returns int to the pow of an int
+ * @x: first parameter, int to be raised
+ * @y: second parameter, power integer
+ * Return: x pow y
  */
 
 int _pow_recursion(int x, int y)
