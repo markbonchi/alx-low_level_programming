c559224c8e09ed3c8a29c8b958918e94ccfe17dd
Mark Obi <markbonchi@gmail.com>
2022-07-25 19:11:40 -0700

    Create_array

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+	char *ptr;
+
+	if (size != 0)
+	{
+		ptr = malloc(sizeof(char) * size);
+		if (ptr == NULL)
+		{
+			return (NULL);
+		}
+		for (i = 0; i < size; i++)
+		{
+			ptr[i] = c;
+		}
+	} else
+	{
+		return (NULL);
+	}
+	return (ptr);
+}
