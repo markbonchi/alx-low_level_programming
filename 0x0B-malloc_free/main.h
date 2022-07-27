6cbdaf6c227a3f2fbfed0078cce0066b47edcc2f
Mark Obi <markbonchi@gmail.com>
2022-07-26 19:42:27 -0700

    Argstostr

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -5,11 +5,11 @@
 #include <string.h>
 
 int _putchar(char c);
+int _strlen(char *s);
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
 int **alloc_grid(int width, int height);
 char *argstostr(int ac, char **av);
-int _strlen(char *s);
 
 #endif
