36fb24a66c2a23f1466de4e68b22262e31b6d6cd
Mark Obi <markbonchi@gmail.com>
2022-07-29 14:54:49 -0700

    string_nconcat

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * string_nconcat - concatenates two strings
+ * @s1: string one
+ * @s2: string two
+ * @n: int number of bytes for string two
+ * Return: ptr
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	int i = 0, j, len;
+	char *ptr;
+
+	if (*s1 == NULL)
+		*s1 = "";
+	if (*s2 == NULL)
+		*s2 = ""
+	len = n;
+	for (; s1[i] != '\0'; i++)
+		len += 1;
+
+	ptr = malloc(sizeof(char) * (len + 1));
+	if (ptr == NULL)
+		return (NULL);
+
+	j = 0;
+	for (; s1[i] != '\0'; i++)
+		ptr[j++] = s1[i];
+	for (; i < n; i++)
+		ptr[j++] = s2[i];
+	ptr[j] = '\0';
+	return (ptr);
+}
