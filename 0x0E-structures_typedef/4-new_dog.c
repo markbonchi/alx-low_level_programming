4e40b9a19b1716a0c1caa026c782f50876fb8534
Mark Obi <markbonchi@gmail.com>
2022-08-02 14:47:34 -0700

    New dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -15,7 +15,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *dog;
 
-	dog = malloc(sizeof(dog->name) + sizeof(dog->owner));
+	dog = malloc(sizeof(dog_t));
 	if (!dog)
 	{
 		free(dog);
