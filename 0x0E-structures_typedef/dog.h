b5538aaf8899f5ec843f9ba99b9c3d8e06fc9369
Mark Obi <markbonchi@gmail.com>
2022-08-02 10:53:33 -0700

    New struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,14 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - poppy
+ * @name: char element
+ * @owner: char owner
+ * @age: int element
+ * Desciption: A dog
+ */
+
 struct dog
 {
 	char *name;
