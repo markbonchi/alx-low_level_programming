185a1ae4f14aca273a421a798d302c164a098f88
Mark Obi <markbonchi@gmail.com>
2022-08-04 14:11:51 -0700

    Header update print_numbers

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,27 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stddef.h>
+
+/**
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	int i, num;
+	va_list vp;
+
+	va_start(vp, n);
+	for (i = 0; i < (int)n; i++)
+	{
+		num = va_arg(vp, int);
+		printf("%d", num);
+		if ((i + 1) != (int)n)
+		{
+			if (separator != NULL)
+				printf("%s", separator);
+			printf(" ");
+		}
+	}
+	va_end(vp);
+	printf("\n");
+}
