992ad8ae16b02f4753ac32b6f3bf41119785c8b2
Mark Obi <markbonchi@gmail.com>
2022-08-04 15:43:44 -0700

    Header update print_strings

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,38 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stddef.h>
+
+/**
+ * print_numbers - prints a list of numbers
+ * @separator: string inbetween
+ * @n: int parameter
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	int i;
+	char *ptr;
+	va_list vp;
+
+	if (n <= 0)
+		return;
+
+	va_start(vp, n);
+	for (i = 0; i < (int)n; i++)
+	{
+		ptr = va_arg(vp, char *);
+		if (!ptr)
+			printf("(nil)");
+		else
+			printf("%s", ptr);
+		if ((i + 1) != (int)n)
+		{
+			if (separator != NULL)
+				printf("%s", separator);
+			else
+				printf(" ");
+		}
+	}
+	va_end(vp);
+	printf("\n");
+}
