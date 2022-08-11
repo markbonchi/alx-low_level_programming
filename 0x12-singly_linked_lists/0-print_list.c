f12735436d47db8e4fbb690f8eb06539d4d03476
Mark Obi <markbonchi@gmail.com>
2022-08-11 11:30:06 -0700

    New header print_list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t i = 0;
+	const list_t *ptr = malloc(sizeof(list_t));
+
+	if (h == NULL)
+	{
+		return (0);
+	}
+
+	ptr = h;
+	while (ptr != NULL)
+	{
+		if (ptr->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", ptr->len, ptr->str);
+		ptr = ptr->next;
+		i++;
+	}
+	return (i);
+}
