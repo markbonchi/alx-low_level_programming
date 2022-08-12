f4c5d1fc0c0d25ef061de014cd88e55de32bf078
Mark Obi <markbonchi@gmail.com>
2022-08-12 16:29:47 -0700

    First

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -8,5 +8,9 @@ void myFirst(void)__attribute__((constructor));
 
 void myFirst(void)
 {
-	printf("You're beat! and yet, you must allow, \nI bore my house upon my back!\n");
+	char *str1, *str2;
+
+	str1 = "You're beat! and yet, you must allow,\n";
+	str2 = "I bore my house upon my back!\n";
+	printf("%s%s", str1, str2);
 }
