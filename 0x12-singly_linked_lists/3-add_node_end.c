c599fc3cd448157ce6c645efa5bc7a0689576dbb
Mark Obi <markbonchi@gmail.com>
2022-08-11 17:25:54 -0700

    Update to header add_node_end

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include <stddef.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+ */
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+	size_t i = 0;
+	list_t *ptr, *temp;
+
+	while (str[i] != '\0')
+		i++;
+
+	temp = malloc(sizeof(list_t));
+	if (!temp)
+		exit(EXIT_FAILURE);
+
+	temp->str = strdup(str);
+	temp->len = i;
+
+	if (*head == NULL)
+	{
+		temp->next = NULL;
+		*head = temp;
+	} else
+	{
+		temp->next = NULL;
+		ptr = *head;
+		while (ptr->next != NULL)
+			ptr = ptr->next;
+		ptr->next = temp;
+	}
+	return (temp);
+}
