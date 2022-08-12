d7caa0b4b16833313282626ae82de4dfbebf6369
Mark Obi <markbonchi@gmail.com>
2022-08-12 16:06:52 -0700

    First

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 
 /**
+ * myFirst - prints before main
  */
 
 void myFirst(void)__attribute__((constructor));
 
 void myFirst(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\nI bore my house upon \
+my back!\n");
 }
