8af6a534367eb82b65b4f6eaa6ae1d8da0bc90cd
Mark Obi <markbonchi@gmail.com>
2022-08-16 05:43:12 -0700

    Free listint 2

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -3,11 +3,16 @@
 #include "lists.h"
 
 /**
+ * free_listint2 - frees a listint_t list
+ * @head: pointer to head pointer
  */
 
 void free_listint2(listint_t **head)
 {
 	listint_t *ptr = NULL;
+
+	if (!*head)
+		*head = NULL;
 
 	while (*head != NULL)
 	{
