135ce9292502704b1f7590accacbd312660e4166
Mark Obi <markbonchi@gmail.com>
2022-08-16 13:25:06 -0700

    Get nodeint

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -23,7 +23,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	{
 		while (head)
 		{
-			if (count == index || ptr->next == NUL)
+			if (count == index || ptr->next == NULL)
 				break;
 			ptr = ptr->next;
 			count++;
