a866c97b38f4b01771178989c4d2cb7412b3ccbb
Mark Obi <markbonchi@gmail.com>
2022-08-22 22:02:01 -0700

    Fix to 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -26,7 +26,7 @@ int create_file(const char *filename, char *text_content)
 		write(STDERR_FILENO, fail, _strlen(fail));
 		return (-1);
 	}
-	file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 600);
+	file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 	if (file == -1)
 	{
 		write(STDERR_FILENO, fail, _strlen(fail));
