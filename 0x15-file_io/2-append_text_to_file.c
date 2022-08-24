9e6e0ec0fef110b5674ea8aee64d8dfce3883d82
Mark Obi <markbonchi@gmail.com>
2022-08-24 06:20:00 -0700

    Update to 1

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -4,11 +4,20 @@
 #include "main.h"
 
 /**
+ * _strlen - finds the length of a string
+ * @str: string parameter
+ *
+ * Return: len
+ */
+
+int _strlen(char *str);
+
+/**
  * append_text_to_file - function that appends text to a file
  * @filename: Name of text file
  * @text_content: string
  *
- * Return: 1 (Success)|| -1 (Failure)
+ * Return: 1 (Success) || -1 (Failure)
  */
 
 int _strlen(char *str);
@@ -16,6 +25,7 @@ int _strlen(char *str);
 int append_text_to_file(const char *filename, char *text_content)
 {
 	int file, write_file;
+	char *fail = "fails"
 
 	if (filename == NULL)
 		return (-1);
@@ -34,7 +44,7 @@ int append_text_to_file(const char *filename, char *text_content)
 		return (1);
 	}
 	close(file);
-	return (-1);
+	return (1);
 }
 
 /**
@@ -43,6 +53,7 @@ int append_text_to_file(const char *filename, char *text_content)
  *
  * Return: len
  */
+
 int _strlen(char *str)
 {
 	int len = 0;
