181e8fe9b3bf827d9b67979e5ac43e361b4867ee
Mark Obi <markbonchi@gmail.com>
2022-08-24 06:09:52 -0700

    Update to 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -19,32 +19,23 @@ int _strlen(char *str);
 int create_file(const char *filename, char *text_content)
 {
 	int file, write_file;
-	char *fail = "fails";
 
 	if (filename == NULL)
-	{
-		write(STDERR_FILENO, fail, _strlen(fail));
 		return (-1);
-	}
 	file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 	if (file == -1)
-	{
-		write(STDERR_FILENO, fail, _strlen(fail));
 		return (-1);
-	}
 	if (text_content != NULL)
 	{
 		write_file = write(file, text_content, _strlen(text_content));
 		if (write_file == -1)
 		{
-			write(STDERR_FILENO, fail, _strlen(fail));
 			close(file);
 			return (-1);
 		}
 		close(file);
 		return (1);
 	}
-	write(STDERR_FILENO, fail, _strlen(fail));
 	close(file);
 	return (-1);
 }
