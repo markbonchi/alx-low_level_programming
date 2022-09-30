115314a064056ef06f17f64b025b9912057a849e
Mark Bonchi <markbonchi@gmail.com>
2022-09-29 17:30:07 -0700

    Get node at index

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -17,6 +17,10 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 
 	temp = head;
 	for (count = 0; count != index; count++)
+	{
+		if (temp == NULL)
+			break;
 		temp = temp->next;
+	}
 	return (temp);
 }
