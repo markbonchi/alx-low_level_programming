f360923c3256b4aeb3606669248f074f3e3de828
Mark Bonchi <markbonchi@gmail.com>
2022-09-30 19:25:08 -0700

    Insert node

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -14,7 +14,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *new_node, *temp, *temp1;
 
 	if (!(*h))
-		return (NULL);
+		new_node = make_node(n);
 
 	if (idx == 0)
 		new_node = add_dnodeint(h, n);
