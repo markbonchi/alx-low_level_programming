1a49cb54aac299562909941bc9bcda03aff36530
Mark Bonchi <markbonchi@gmail.com>
2022-10-21 19:17:46 -0700

    Hash table

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,26 @@
+#include "hash_tables.h"
+
+/**
+ */
+
+hash_table_t *hash_table_create(unsigned int size)
+{
+	unsigned int i;
+	hash_table_t *table;
+	hash_node_t **arr;
+
+	table = malloc(sizeof(hash_table_t));
+	if (table == NULL)
+		return (NULL);
+
+	arr = malloc(sizeof(hash_node_t *) * size);
+	if (arr == NULL)
+		return (NULL);
+
+	for(i = 0; i < size; i++)
+		arr[i] = NULL;
+
+	table->array = arr;
+	table->size = size;
+	return (table);
+}
